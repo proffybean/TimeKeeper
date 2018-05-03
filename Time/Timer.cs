cafba3776aecd7029f1827ed5a580459d58cd127
Unknown <proffy@comcast.net>
2018-05-03 11:08:47 -0700

    Initial Commit

---
+++ Time/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Time
+{
+    public class Timer
+    {
+        public TimeSpan Measure(Action action)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            action();
+            return watch.Elapsed;
+        }
+    }
+}
